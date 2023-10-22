49fa0afa6e35a9f2891537bd09e9bf78705ad4b9
RomanBoegli <roman.bogli@outlook.com>
2023-10-22 19:47:22 +0200

    .

--- gostark_test.go
+++ gostark_test.go
@@ -1,13 +1,15 @@
-package gostark
+package gostark_test
 
 import (
 	"testing"
+
+	"github.com/RomanBoegli/gostark"
 )
 
 // TestHelloName calls greetings.Hello with a name, checking
 // for a valid return value.
 func TestHelloName(t *testing.T) {
-	is := Average(3.0, 5.0)
+	is := gostark.Average(3.0, 5.0)
 	should := 4.0
 	if is != should {
 		t.Fatalf(`Average(3, 5) returned %v but should be %v`, is, should)
